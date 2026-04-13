c1bc81bd01a6afd4a930efa50b9c9c8875bb90a6
DMGTTRANS <juridico.transito@gravata.pe.gov.br>
2026-04-13 12:37:43 -0300

    Update document-completed.tsx

--- packages/email/templates/document-completed.tsx
+++ packages/email/templates/document-completed.tsx
@@ -39,7 +39,7 @@ export const DocumentCompletedEmailTemplate = ({
                 <Img src={branding.brandingLogo} alt="Branding Logo" className="mb-4 h-6" />
               ) : (
                 <Img
-                  src={getAssetUrl('/static/logo.png')}
+                  src={getAssetUrl('/logo-orgao.png')}
                   alt="Documenso Logo"
                   className="mb-4 h-6"
                 />
